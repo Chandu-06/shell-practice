30c198573df4935998be0cbc618f025e22376ead
Devineni Pranay Chandu <chandudevineni06@gmail.com>
2025-10-14 19:13:09 +0530

    Special vars

--- spec_var-shell.sh
+++ spec_var-shell.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed to the Script: $@"
 echo "No of variables: $#"
+echo "Script name: $0"
+echo "present working directory: $PWD"
+echo "home directory of the user who is running the script: $HOME"
+echo "which user is running the script: $USER"
+echo "PID of the current script: $$"
+sleep 10 &
+echo "PID of the last command running in background: $!"
